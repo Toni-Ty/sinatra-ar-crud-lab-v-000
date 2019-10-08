227e39c165ddf020dc72b8c36615395f8f981ed5
Toni-Ty <antonique2479@yahoo.com>
2019-10-08 00:21:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
-end
+
+  #creates POST form
+  get '/articles/new' do
+    erb :new
+  end
+
+# index articles
+  post '/articles' do
+      Post.create(params)
+      redirect '/posts'
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #renderd update form
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+
+  patch '/articles/:id'  do
+    id = params["id"]
+    new_params = {}
+    old_post = Post.find(id)
+    new_params[:name] = params["name"]
+    new_params[:content] = params["content"]
+    old_post.update(new_params)
+
+    redirect "/posts/#{id}"
+    end
+
+    delete '/articles/:id/delete' do
+      @article = Post.find(params[:id])
+      @article.delete
+      erb :delete
+    end
+  end
