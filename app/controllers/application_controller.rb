dc529f55c6641b9527b8f3a383f62cbe7d347e12
Toni-Ty <antonique2479@yahoo.com>
2019-10-08 01:11:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/articles' do
-      Article.create(params)
+      Article.create(params[:content])
       redirect '/posts'
     erb :show
   end
