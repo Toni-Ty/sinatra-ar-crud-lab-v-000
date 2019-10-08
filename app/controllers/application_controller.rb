f0b04d0847b9784f447f2439386feff4443711a5
Toni-Ty <antonique2479@yahoo.com>
2019-10-08 04:27:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ end
 
   patch '/articles/:id'  do
     @article = Article.find_by_id(params[:id])
-    @article.name = params[:name]
+    @article.title = params[:title]
     @article.content = params[:content]
     @article.save
     erb :show
